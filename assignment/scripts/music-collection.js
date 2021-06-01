8b231ae64887f7b544d9fdaf5ea3d8dc016abfd8
Benji Copan <benji1220@gmail.com>
2021-06-01 11:24:05 -0500

    create new object having input parameters as properties

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -3,5 +3,9 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection(title, artist, yearPublished){
-
-}
+  let record = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  } //end object record
+} //end function addToCollection

ce0f9bd185d0251cd25c87e00ac70248bdb25793
Benji Copan <benji1220@gmail.com>
2021-06-01 13:52:40 -0500

    create and robustly test function that searches collection for all albums by a given artist and returns them in an array

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -35,6 +35,12 @@ console.log("Add to collection: Swans, The Seer, 2012", addToCollection("Swans",
 console.log("Updated collection (6 records)", collection);
 console.log("Add to collection: Bob Dylan, The Freewheelin' Bob Dylan, 1963", addToCollection("Bob Dylan", "The Freewheelin' Bob Dylan", 1963));
 //console.log("Updated collection", collection);
+console.log("Add to collection: Bob Dylan, Highway 61 Revisited, 1965", addToCollection("Bob Dylan", "Highway 61 Revisited", 1965));
+//console.log("Updated collection", collection);
+console.log("Add to collection: Bob Dylan, Blood on the Tracks, 1975", addToCollection("Bob Dylan", "Blood on the Tracks", 1975));
+//console.log("Updated collection", collection);
+console.log("Add to collection: Bob Dylan, Rough and Rowdy Ways, 2020", addToCollection("Bob Dylan", "Rough and Rowdy Ways", 2020));
+//console.log("Updated collection", collection);
 console.log("Add to collection: Patti Smith, Horses, 1975", addToCollection("Patti Smith", "Horses", 1963));
 //console.log("Updated collection", collection);
 console.log("Add to collection: Mitski, Be The Cowboy, 2018", addToCollection("Mitski", "Be The Cowboy", 2018));
@@ -63,10 +69,42 @@ function showCollection(collection){
   //log array length
   console.log(collection.length);
 
-  // loop over array elements, formatting and printing each object
+  //loop over array elements, formatting and printing each object
   for (let record of collection){
     console.log(`${record.title} by ${record.artist}, published in ${record.yearPublished}`);
   }
-} //end function showCollection
+ } //end function showCollection
 
+//test showCollection
 showCollection(collection);
+
+//find all albums in a collection by a particular artist
+//(I know the instructions didn't tell us to take the collection as an input, but I figured this way we can use the same function to search someone else's collection too)
+function findByArtist(artist, collection){
+  let albumsByArtist = [];
+  //go through record collection one at a time
+  for(let record of collection){
+    //compare input artist to artist of current record. trim string and convert to lower case so that whitespace and capitalization don't have to match
+    if( artist.trim().toLowerCase() == record.artist.trim().toLowerCase() ) {
+      albumsByArtist.push(record);
+    }
+  }
+  //return array of successful matches
+  return albumsByArtist;
+} //end function findByArtist
+
+//search for William Shatner - no results
+console.log("Find albums by William Shatner (none):");
+showCollection(findByArtist("William Shatner", collection));
+
+//search for GZA - one result
+console.log("Find albums by GZA (one):");
+showCollection(findByArtist("GZA", collection));
+
+//search for Harry Nilsson - unsanitized input, one result
+console.log("Find albums by '  hArRy nIlSSoN  ' (with Spongebobbed capitalization and unnecessary whitespace - should find oneone):");
+showCollection(findByArtist("  hArRy nIlSSoN  ", collection));
+
+//search for Bob Dylan - four results
+console.log("Find albums by Bob Dylan (four):");
+showCollection(findByArtist("Bob Dylan", collection));

7d107d49e6510eaf038c889d63a81f8b278e4e1f
Benji Copan <benji1220@gmail.com>
2021-06-01 11:43:28 -0500

    in function addToCollection, push new object onto array collection and return new object. also added comments, tested function, and added question to html

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -2,10 +2,21 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+//function to add records into collection
 function addToCollection(title, artist, yearPublished){
+  //create record object with input properties
   let record = {
     title: title,
     artist: artist,
     yearPublished: yearPublished
   } //end object record
+
+  //push newly created record onto collection array
+  collection.push(record);
+
+  //return new records
+  return record;
 } //end function addToCollection
+
+//console.log(`addToCollection("The Beatles", "Abbey Road", 1969): ${addToCollection("The Beatles", "Abbey Road", 1969)}`);
+console.log("add to collection: The Beatles, Abbey Road, 1969", addToCollection("The Beatles","Abbey Road", 1969));
